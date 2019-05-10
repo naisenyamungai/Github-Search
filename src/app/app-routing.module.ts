2ce22530675c1132b48654225ab00e579ca02fb9
naisenyamungai <naisenyamungai@gmail.com>
2019-05-10 09:59:45 +0300

    import Home, About and Contact Components to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [];
 
