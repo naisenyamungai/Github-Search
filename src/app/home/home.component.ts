b5134c85d83db13a02c8993992e43a3d5de32ee9
naisenyamungai <naisenyamungai@gmail.com>
2019-05-10 16:35:06 +0300

    add files to repository class

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { User} from '../user';
+import { Repository } from '../repository';
 
 @Component({
   selector: 'app-home',
--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,10 @@
 export class Repository {
+    constructor(
+        public name:string,
+        public full_name:string,
+        public homepage:string,
+        public description:string,
+       
+    ){
+  } 
 }
